0a27fa90b59b6ba34a62fe673a714bb2d53a4c61
zuyanliang@gmail.com <zuyanliang@gmail.com>
2014-10-13 15:28:15 -0700

    set default map view

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 function initialize() {
+  var hrLat = 39.4438478;  var hrLong = -94.9744697;
+  var myLatLng = new google.maps.LatLng(hrLat, hrLong);
   var mapOptions = {
-    center: { lat: -34.397, lng: 150.644},
-    zoom: 8
+    zoom: 5,
+    center: myLatLng
   };
+
+
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  }
+  
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
+
+// var map = new 
